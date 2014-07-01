f4880f7b24fab95844569e310d8c50f924ff61c5
edvardg <edvard@Edvards-MacBook-Pro.local>
2014-07-02 00:31:12 +0400

    Added custom cell for Category View.

---
+++ Menu/CategoryCustomViewCell.h
@@ -0,0 +1,16 @@
+//
+//  CustomViewCell.h
+//  Menu
+//
+//  Created by Edvard on 6/29/14.
+//  Copyright (c) 2014 Artak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryCustomViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *lblTitle;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
